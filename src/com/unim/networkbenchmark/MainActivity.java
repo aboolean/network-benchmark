9fd2093e260a02314fa2da5d3f47438230aa6d72
Andre Aboulian <aandre@mit.edu>
2014-03-31 03:35:58 -0400

    layout revision

--- src/com/unim/networkbenchmark/MainActivity.java
+++ src/com/unim/networkbenchmark/MainActivity.java
@@ -4,15 +4,24 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
-
+	private Button button;
+	private TextView results, status, ratio, conn, latency, speed, duration;
+	private ProgressBar progress;
+	private Handler handler;
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,6 +31,25 @@ public class MainActivity extends ActionBarActivity {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+		
+		handler = new Handler();
+	}
+	
+	@Override
+	protected void onPostCreate(Bundle b) {
+		super.onPostCreate(b);
+		
+		button = (Button) findViewById(R.id.button1);
+		results = (TextView) findViewById(R.id.out_results);
+		status = (TextView) findViewById(R.id.out_status);
+		ratio = (TextView) findViewById(R.id.out_progress);
+		conn = (TextView) findViewById(R.id.out_conn);
+		latency = (TextView) findViewById(R.id.out_latency);
+		speed = (TextView) findViewById(R.id.out_speed);
+		duration = (TextView) findViewById(R.id.out_time);
+		progress = (ProgressBar) findViewById(R.id.progressBar1);
+		
+		progress.setProgress(0);
 	}
 
 	@Override
@@ -60,5 +88,45 @@ public class MainActivity extends ActionBarActivity {
 			return rootView;
 		}
 	}
+	
+	public void pressButton(View view) {		
+		// clear/set fields
+		button.setEnabled(false);
+		status.setText("Busy");
+		ratio.setText("");
+		latency.setText("");
+		conn.setText("");
+		speed.setText("");
+		duration.setText("");
+		results.setText("");
+		progress.setProgress(0);
+		
+		Thread dl = new Thread(){
+			public void run(){
+				downloadFile();
+			}
+		};
+		dl.start();
+	}
+	
+	public void downloadFile() {
+		// connection type
+		
+		
+		// latency
+		
+		
+		
+		
+		
+		// update UI
+		handler.post(new Runnable() {
+			public void run() {
+				status.setText("Hello");
+			}
+		});
+	}
+	
+	
 
 }
